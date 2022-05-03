d95bc150a6b09d30bd16a8090c6b6f595b186efc
Dei Ren Wu <deirenwu1101@gmail.com>
2022-05-04 01:37:06 +0800

    Setup background image view

--- PodcastDemo/ViewController.swift
+++ PodcastDemo/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configBackImageView()
+        
+        view.backgroundColor = .systemBackground
     }
-
+    
+    private let backImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "xmark")
+        return imageView
+    }()
+    
+    private func configBackImageView() {
+        view.addSubview(backImageView)
+        backImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backImageView.heightAnchor.constraint(equalToConstant: view.frame.width * (9 / 16)),
+            backImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 
 }
 
