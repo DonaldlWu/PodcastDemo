36e526ad0e83a89ae4e6b2138fa1341bc5444bba
Dei Ren Wu <deirenwu1101@gmail.com>
2022-05-06 01:25:41 +0800

    Extract close button layout

--- PodcastDemo/Controllers/PodcastPlayerViewController.swift
+++ PodcastDemo/Controllers/PodcastPlayerViewController.swift
@@ -53,10 +53,15 @@ class PodcastPlayerViewController: UIViewController {
         return btn
     }()
 
-    let closeButton: UIButton = {
+    private let closeButton: UIButton = {
         let btn = UIButton()
         btn.setImage(UIImage(systemName: "xmark"), for: .normal)
         btn.addTarget(self, action: #selector(dismissController), for: .touchUpInside)
+        btn.tintColor = .systemGray
+        btn.layer.borderColor = UIColor.systemGray.cgColor
+        btn.layer.borderWidth = 2
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 22
         return btn
     }()
 
@@ -150,14 +155,7 @@ class PodcastPlayerViewController: UIViewController {
         configAudioLengthLabel()
         configCurrentTimeLabel()
         configDescriptionTextView()
-        view.addSubview(closeButton)
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
-            closeButton.heightAnchor.constraint(equalToConstant: 44),
-            closeButton.widthAnchor.constraint(equalToConstant: 44)
-        ])
+        configCloseButton()
     }
     
     private func configUIContent() {
@@ -365,6 +363,17 @@ extension PodcastPlayerViewController {
             descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 12),
             descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
             descriptionTextView.bottomAnchor.constraint(equalTo: currentTimeLabel.topAnchor, constant: -12)
+        ])
+    }
+    
+    private func configCloseButton() {
+        view.addSubview(closeButton)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
+            closeButton.heightAnchor.constraint(equalToConstant: 44),
+            closeButton.widthAnchor.constraint(equalToConstant: 44)
         ])
     }
 
