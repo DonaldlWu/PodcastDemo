9cf3b5f8b855f2b939edb3a94b41a54627ac4492
Dei Ren Wu <deirenwu1101@gmail.com>
2022-05-06 22:10:22 +0800

    Add table view cell and feed rss title

--- PodcastDemo/Controllers/EpsiodeListViewController.swift
+++ PodcastDemo/Controllers/EpsiodeListViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class EpsiodeListViewController: UITableViewController {
     private var loader: RSSLoader?
+    private var tableModel: RSSItem? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     
     convenience init(loader: RSSLoader?) {
         self.init()
@@ -17,9 +24,8 @@ class EpsiodeListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(loadRssFeed), for: .valueChanged)
+        registerCell()
+        configRefreshControl()
         loadRssFeed()
     }
     
@@ -28,12 +34,21 @@ class EpsiodeListViewController: UITableViewController {
         loader?.load { [weak self] result in
             switch result {
             case let .success(rss):
-                print(rss)
+                self?.tableModel = rss
             case let .failure(error):
                 print(error)
             }
             self?.endRefreshing()
         }
+    }
+    
+    private func registerCell() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    private func configRefreshControl() {
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(loadRssFeed), for: .valueChanged)
     }
     
     private func startRefreshing() {
@@ -47,5 +62,23 @@ class EpsiodeListViewController: UITableViewController {
             self.refreshControl?.endRefreshing()
         }
     }
+}
+
+extension EpsiodeListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let tableModel = tableModel else {
+            return 0
+        }
+        return tableModel.channel.item.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        guard let tableModel = tableModel else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = tableModel.channel.item[indexPath.row].title
+        
+        return cell
+    }
 }
